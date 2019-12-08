55a646ce605a31f46088ce111bbd22156a2bd64c
John Loverich <john.loverich@gmail.com>
2019-12-08 09:08:26 -0800

    Tests now passing.  Need to fix tf messages.

---
+++ NeuralSentenceSearch.js
@@ -0,0 +1,49 @@
+"use strict";
+let use = require('@tensorflow-models/universal-sentence-encoder')
+let tf = require('@tensorflow/tfjs')
+let mobilenetModule = require('@tensorflow-models/mobilenet')
+let knnClassifier = require('@tensorflow-models/knn-classifier/dist/knn-classifier.js')
+let debug = require('debug')('NeuralSentenceSearch')
+
+class NeuralSentenceSearch {
+  constructor() {
+    this.model = null
+    debug('knnClassifier', knnClassifier)
+    this.classifier = knnClassifier.create()
+    this.dictionary = []
+    this.count = 0
+    this.nearestNeighbors = 1
+  }
+
+  async initialize() {
+    this.model = await use.load()
+  }
+
+  /**
+   * Add a bunch of sentences that should be matched to the given object.  The "class" of the object
+   * is generated automatically as the last index of the dictionary.
+   * @param {*} obj is a single object which should be returned if any of the "sentences" is matched.
+   * @param {*} sentences is a list of sentences that should be mapped to the object.
+   */
+  async addSameClass(obj, sentences) {
+    this.dictionary.push(obj)
+    
+    let theseSentences = (sentences instanceof Array) ? sentences : [sentences]
+    let embeddings = await this.model.embed(theseSentences)
+
+    console.log("embeddings",embeddings)
+    let c = theseSentences.map((s,i)=>{
+      this.classifier.addExample(embeddings.slice([i, 0], [1, 512]), this.dictionary.length-1)
+    })
+
+  }
+
+  async search(val) {
+    let example = await this.model.embed(val)
+    let ans = await this.classifier.predictClass(example, this.nearestNeighbors)
+    return { result : ans, object : this.dictionary[ans.classIndex] }
+  }
+}
+
+module.exports = NeuralSentenceSearch
+//export default NeuralSentenceSearch
---
+++ __tests__/NeuralSentenceSearchTest.js
@@ -0,0 +1,23 @@
+"use strict";
+let NeuralSentenceSearch = require("../NeuralSentenceSearch.js")
+
+test('Add some examples', async () => {
+  let nn = new NeuralSentenceSearch()
+
+  await nn.initialize();
+
+  await nn.addSameClass({a : "firstClass"}, "This is a test")
+  await nn.addSameClass({a : "secondClass"}, ["My name is John", "Me llamo Sarah"])
+  await nn.addSameClass({d : {c : "the last class"}}, ["In the galaxy", "solar system"])
+
+  let ans1 = await nn.search("the sun")
+  expect(ans1.result.classIndex).toBe(2)
+  expect(ans1.object.d.c).toBe('the last class')
+  console.log('ans', ans1)
+
+  let ans2 = await nn.search("his name is Jerry")
+  expect(ans2.result.classIndex).toBe(1)
+  expect(ans2.object.a).toBe('secondClass')
+  console.log('ans', ans2)
+
+});
